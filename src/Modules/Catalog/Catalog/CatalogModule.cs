ccc0d44d93516566b471b1d15c5415616b392d32
Jaffar Abbas <gamaportal8@gmail.com>
2025-01-25 00:24:48 +0500

    added service piple on modules

---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogModule;
+public static class CatalogModule
+{
+    public static IServiceCollection AddCatalogModule(this IServiceCollection services,IConfiguration confriguation)
+    {
+        return services;
+    }
+}
